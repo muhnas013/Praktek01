df9a41e24afd5d440497a97967afbda445599487
Muhammad Nasrullah <muhammad.nasrullah1993@gmail.com>
2023-05-25 20:56:32 +0800

    Membuat Atribut dan Behavior

--- src/main/java/pbo/praktek01/Mahasiswa.java
+++ src/main/java/pbo/praktek01/Mahasiswa.java
@@ -6,5 +6,13 @@ package pbo.praktek01;
 
 
 public class Mahasiswa {
+    String nama;
+    String NPM;
+    double IPK;
     
+    void cetakInfo () {
+        System.out.println("Nama :"+nama);
+        System.out.println("NPM :"+NPM);
+        System.out.println("IPK :"+IPK);
+    }
 }
