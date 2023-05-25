b367424d03ce4e589a016fb8cb9bcd304eb89914
Muhammad Nasrullah <muhammad.nasrullah1993@gmail.com>
2023-05-25 21:08:34 +0800

    Membuat Class Aksi dan Method Main

---
+++ src/main/java/pbo/praktek01/mahasiswaAksi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo.praktek01;
+
+/**
+ *
+ * @author damn
+ */
+public class mahasiswaAksi {
+    public static void main(String[] args) {
+        
+    }
+vm
+}
